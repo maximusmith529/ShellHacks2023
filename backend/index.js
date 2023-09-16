a5ad566f9e633dcfc4db2b7ee74911f1a7d19149
maximusmith529 <maximusmith529@gmail.com>
2023-09-16 17:35:05 -0400

    edited replicated function names and removed some irrelevant code

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,7 @@
 //Shellhacks 2023
 //Backend team: Seth Lenhof, Maximus Smith
 
-async function sendMessage(message) {
+async function askGPT(message) {
   const systemPrompt = "You are a Dungeon Master for a table-top based game that you created. Speak to me as if I was your player. The game system should be dungeons and dragons based with rolls to determine outcomes.\n\nThe dungeon master should ask before rolling and then generate a random roll with proficiencies. \n\nWhen describing the game and assisting the player in non-cinematic or non-roleplaying scenarios your responses should be short and concise. Otherwise, when describing scenes and dialogues you can use more words, but again do not add too many extraneous words.\n\nThe setting in which you have the game should be low fantasy\n\nYour responses should follow this format. Do not repeat (OPTIONAL) values that do not change for example the stats.\n\n{\n    \"messageToPlayer\": \"String\",\n    \"requiredRollStat + DC (OPTIONAL)\": \"String\",\n    \"potentialActions (OPTIONAL) [MAX LENGTH: 3]\": [\n        \"STRING\"\n    ],\n    \"environmentalEffects\": {\n        \"inForest (OPTIONAL)\": \"Boolean\",\n        \"inCave (OPTIONAL)\": \"Boolean\",\n        \"inTown (OPTIONAL)\": \"Boolean\",\n        \"inDungeon (OPTIONAL)\": \"Boolean\",\n        \"lightLevel (OPTIONAL)\": \"Int\",\n        \"isRaining (OPTIONAL)\": \"Boolean\",\n        \"isSnowing (OPTIONAL)\": \"Boolean\"\n    },\n    \"characterStats\":{\n        \"strength (OPTIONAL)\": \"Int\",\n        \"health (OPTIONAL)\": \"Int\",\n        \"intelligence (OPTIONAL)\": \"Int\",\n        \"dexterity (OPTIONAL)\": \"Int\",\n        \"charisma (OPTIONAL)\": \"Int\",\n        \"wisdom (OPTIONAL)\": \"Int\",\n        \"constitution (OPTIONAL)\": \"Int\"\n    }\n}";
   const requestBody = {
       model: "gpt-4",
@@ -15,6 +15,7 @@ async function sendMessage(message) {
   };
   // adds user message to chatScreen
   messageBuilder(newMessage('user', message));
+  sendMessage();
   try {
       const response = await fetch('https://api.openai.com/v1/chat/completions', {
           method: 'POST',
@@ -30,6 +31,7 @@ async function sendMessage(message) {
 
       // adds to chatScreen
       messageBuilder(newMessage('system',data.choices[0].message.content.messageToPlayer))
+      sendMessage();
       return data.choices[0].message;
   } catch (error) {
   console.error('Error calling the ChatGPT API:', error);
@@ -37,7 +39,7 @@ async function sendMessage(message) {
 }
 }
 
-async function main() {
-  const answer = await sendMessage("hello");
-  console.log(answer);
-}
+// async function main() {
+//   const answer = await sendMessage("hello");
+//   console.log(answer);
+// }
