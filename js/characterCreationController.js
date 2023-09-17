a264ac20973e2f179be548a9ab8bdc58ea4cbd3c
Alex Harris <72664735+HarrisAlex@users.noreply.github.com>
2023-09-16 20:33:20 -0400

    Created character creation screen

---
+++ js/characterCreationController.js
@@ -0,0 +1,175 @@
+let races = [
+    "Dragonborn",
+    "Dwarf",
+    "Elf",
+    "Gnome",
+    "Half-Elf",
+    "Halfling",
+    "Half-Orc",
+    "Human",
+    "Tiefling"
+]
+
+let classes = [
+    "Barbarian",
+    "Bard",
+    "Cleric",
+    "Druid",
+    "Fighter",
+    "Monk",
+    "Paladin",
+    "Ranger",
+    "Rogue",
+    "Sorcerer",
+    "Warlock",
+    "Wizard"
+]
+
+let stats = [
+    "Strength",
+    "Dexterity",
+    "Constitution",
+    "Intelligence",
+    "Wisdom",
+    "Charisma"
+]
+
+let classesContainer = $("#characterClassButtons");
+let racesContainer = $("#characterRaceButtons");
+let statsContainer = $("#statFieldsContainer");
+let pointsLeft = $("#pointsLeft");
+
+let currentClass = "";
+let currentRace = "";
+
+for (let i = 0; i < classes.length; i++) {
+    let button = $("<div>");
+    button.addClass("button");
+    button.text(classes[i]);
+    classesContainer.append(button);
+
+    button.click(function() {
+        setActiveClass(button);
+    });
+}
+
+for (i = 0; i < races.length; i++) {
+    let button = $("<div>");
+    button.addClass("button");
+    button.text(races[i]);
+    racesContainer.append(button);
+
+    button.click(function() {
+        setActiveRace(button)
+    });
+}
+
+for (i = 0; i < stats.length; i++) {
+    let stat = $("<div>");
+    stat.addClass("statField");
+    stat.attr("value", 10);
+    stat.attr("id", stats[i]);
+    statsContainer.append(stat);
+
+    let label = $("<label>");
+    label.text(stats[i]);
+    stat.append(label);
+
+    let statValue = $("<div>");
+    statValue.addClass("statValueField");
+    stat.append(statValue);
+
+    let statValueText = $("<div>");
+    statValueText.text(stat.attr("value"));
+
+    let decrement = $("<div>");
+    decrement.addClass("button");
+    decrement.text("-");
+    decrement.click(function() {
+        decrementStat(stat, statValueText);
+    });
+    statValue.append(decrement);
+   
+    statValue.append(statValueText);
+
+    let increment = $("<div>");
+    increment.addClass("button");
+    increment.text("+");
+    increment.click(function() {
+        incrementStat(stat, statValueText);
+    });
+    statValue.append(increment);
+}
+
+let characterInformation;
+
+$("#characterCreateButton").click(function() {
+    characterInformation = {
+        name: $("#characterNameInput").val(),
+        class: currentClass,
+        race: currentRace,
+        stats: {
+            strength: $("#Strength").attr("value"),
+            dexterity: $("#Dexterity").attr("value"),
+            constitution: $("#Constitution").attr("value"),
+            intelligence: $("#Intelligence").attr("value"),
+            wisdom: $("#Wisdom").attr("value"),
+            charisma: $("#Charisma").attr("value")
+        }
+    }
+
+    $("#characterCreationScreen").hide();
+});
+
+function setActiveClass(selection) {
+    $(selection).addClass("selected");
+    $(selection).siblings().removeClass("selected");
+    currentClass = $(selection).text();
+}
+
+function setActiveRace(selection) {
+    $(selection).addClass("selected");
+    $(selection).siblings().removeClass("selected");
+    currentRace = $(selection).text();
+}
+
+let availablePoints = 20;
+updatePointsLeft();
+
+function decrementStat(selection, text) {
+    if (selection.attr("value") < 9)
+        return;
+
+    let cost = 1;
+    if (selection.attr("value") > 14)
+        cost++;
+
+    selection.attr("value", parseInt(selection.attr("value"), 10) - 1);
+    text.text(selection.attr("value"));
+    availablePoints += cost;
+    
+    updatePointsLeft();
+}
+
+function incrementStat(selection, text) {
+    if (selection.attr("value") > 19)
+        return;
+
+    let cost = 1;
+    if (selection.attr("value") > 14)
+        cost++;
+
+    if (availablePoints < cost)
+        return;
+
+    selection.attr("value", parseInt(selection.attr("value"), 10) + 1);
+    text.text(selection.attr("value"));
+    availablePoints -= cost;
+
+    updatePointsLeft();
+}
+
+function updatePointsLeft()
+{
+    pointsLeft.text("Points left: " + availablePoints);
+}
